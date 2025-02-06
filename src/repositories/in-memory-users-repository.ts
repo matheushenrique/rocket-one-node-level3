60915358bbbec622cc68cff8c9adf6e4213cd131
matheushenrique <matheus.henriquepr@gmail.com>
2025-02-06 00:35:22 -0300

    inversion of dependency

---
+++ src/repositories/in-memory-users-repository.ts
@@ -0,0 +1,7 @@
+import { Prisma } from '@prisma/client'
+export class InMemoryUsersRepository {
+  public users: any = []
+  async create(data: Prisma.UserCreateInput) {
+    this.users.push(data)
+  }
+}
