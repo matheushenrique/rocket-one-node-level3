e68fe651850de5644f5784b18a57293b08f4001e
matheushenrique <matheus.henriquepr@gmail.com>
2025-02-12 00:20:20 -0300

    late check-in validation

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ GymPass style app
 - [x] The user should not be able to register with a duplicate email;
 - [x] The user cannot do 2 check-in on the same day;
 - [x] The user cannot check in if not close (100m) from the gym;
-- [ ] Check-in can only be validated up to 20 minutes after being created;
+- [x] Check-in can only be validated up to 20 minutes after being created;
 - [ ] Check-in can only be validated by administrators;
 - [ ] The gym can only be registered by administrators;
 
--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,9 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { LateCheckInValidationError } from '@/use-cases/errors/late-check-in-validation-error'
 import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
 import { CheckIn } from '@prisma/client'
+import dayjs from 'dayjs'
+
 interface ValidateCheckInUseCaseRequest {
   checkInId: string
 }
@@ -18,6 +21,15 @@ export class ValidateCheckInUseCase {
     if (!checkIn) {
       throw new ResourceNotFoundError()
     }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
+    }
+
     checkIn.validated_at = new Date()
     await this.checkInsRepository.save(checkIn)
     return {
